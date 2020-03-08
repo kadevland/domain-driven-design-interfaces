0203014f561aee4605990a39d262002c0bfb50a8
kaosland <kaosland@kadevland.net>
2020-03-08 23:10:24 +0100

    feat(Entity): create interface Entity

---
+++ src/Entity.php
@@ -0,0 +1,28 @@
+<?php
+namespace kadev\DDD\interfaces;
+
+/**
+ *  Implement Entity concept of domain driven design
+ * 
+ *  @author Kadevland
+ * 
+ * @package kadev\DDD\interfaces
+ */
+interface Entity
+{
+ 
+    /**
+     * Returns the property identity of the Entity
+     *  
+     * @return mixed 
+     */
+    public function identity();
+
+    /**
+     * Checks whether the Entity represented by this object equals to another Entity.
+     * 
+     * @param Entity 
+     * @return bool 
+     */
+    public function equals(Entity $other):bool;
+}
